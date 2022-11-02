10135b9d0ab0524d96ab928d8621bc4a2bfa1270
malan <malanakgames@seznam.cz>
2022-11-02 21:29:53 +0100

    Now Works! ...Kinda...

--- formHandler.js
+++ formHandler.js
@@ -4,41 +4,56 @@ var curDiv = 0;
   // Add 'submit' event handler
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    curDiv += 1;
     const newDiv = document.createElement("div");
     const newIdDiv = document.createElement("div");
     const newInfFromDiv = document.createElement("div");
     const newNotesDiv = document.createElement("div");
     const newUserNameDiv = document.createElement("div");
+    const newActionBtn = document.createElement("button");
     const newStatusDiv = document.createElement("div");
-    newIdDiv.setAttribute('id', newId('ID', curDiv));
-    newInfFromDiv.setAttribute('id', newId('infFrom', curDiv));
-    newNotesDiv.setAttribute('id', newId('notes', curDiv));
-    newUserNameDiv.setAttribute('id', newId('userName', curDiv));
-    newStatusDiv.setAttribute('id', newId('status', curDiv));
+    newIdDiv.setAttribute('id', "ID" + curDiv);
+    newIdDiv.setAttribute('class', 'speDiv');
+    newInfFromDiv.setAttribute('id', "infFrom" + curDiv);
+    newInfFromDiv.setAttribute('class', 'speDiv');
+    newNotesDiv.setAttribute('id', 'notes' + curDiv);
+    newNotesDiv.setAttribute('class', 'speDiv');
+    newUserNameDiv.setAttribute('id', 'userName' + curDiv);
+    newUserNameDiv.setAttribute('class', 'speDiv');
+    newActionBtn.setAttribute('id', 'actButton');
+    newActionBtn.setAttribute('class', 'actButton');
+    newActionBtn.setAttribute('onclick', 'bringUp(' + curDiv + ");");
+    newActionBtn.appendChild(document.createTextNode('Action Do'));
+    newStatusDiv.setAttribute('id', 'status' + curDiv);
+    newStatusDiv.setAttribute('class', 'speDiv');
     newDiv.appendChild(newIdDiv);
     newDiv.appendChild(newInfFromDiv);
     newDiv.appendChild(newNotesDiv);
     newDiv.appendChild(newUserNameDiv);
+    newDiv.appendChild(newActionBtn);
     newDiv.appendChild(newStatusDiv);
-    curDiv += 1;
     tabContent.appendChild(newDiv);
     console.log("Done");
     const FData = new FormData(form);
     addType(newId("ID", curDiv),"p" , "id", FData);
-    addType(newId("infFrom", curDiv), "p", "infFrom", FData)
-    addType(newId("notes", curDiv), "p", "notes", FData)
-    addType(newId("userName", curDiv), "p", "userName", FData)
-    addType(newId("status", curDiv), "p", "status", FData)
+    addType(newId("infFrom", curDiv), "p", "infFrom", FData);
+    addType(newId("notes", curDiv), "p", "notes", FData);
+    addType(newId("userName", curDiv), "p", "username", FData);
+    addType(newId("status", curDiv), "p", "status", FData);
 })
 
-function addType(id, typeToAdd, formId, FData) {
+function addType(id, typeToAdd, formId, FData,) {
     const newOne = document.createElement(typeToAdd);
-    const now = document.getElementById(id);
-    newOne.appendChild(document.createTextNode(FData.get(formId)))
+    const now = document.getElementById(String(id));
+    newOne.appendChild(document.createTextNode(FData.get(formId)));
     now.appendChild(newOne);
 }
 
 function newId(og, curDiv) {
-    newOne = toString(og) + toString(curDiv)
-    return toString(newOne)
+    newOne = og + curDiv
+    return newOne
+}
+
+function bringUp(id) {
+    console.log("brang up" + id)
 }
