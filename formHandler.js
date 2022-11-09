027258c337d2aa28019974fda3f85c52cf756d9a
malan <malanakgames@seznam.cz>
2022-11-09 14:25:25 +0100

    Added ID Back, no longer .html required in url

--- formHandler.js
+++ formHandler.js
@@ -1,52 +1,58 @@
 const form = document.getElementById("devForm");
 const tabContent = document.getElementById("tabContent");
-var curId = 100;
 const page = location.protocol;
+let idUsed = [];
   // Add 'submit' event handler
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const FData = new FormData(form);
-    curId += 1;
-    const newDiv = document.createElement("div");
-    const newIdDiv = document.createElement("div");
-    const newInfFromDiv = document.createElement("div");
-    const newNotesDiv = document.createElement("div");
-    const newUserNameDiv = document.createElement("div");
-    const newActionBtn = document.createElement("button");
-    const newStatusDiv = document.createElement("div");
-    newDiv.setAttribute('id', 'hackList')
-    newIdDiv.setAttribute('id', "ID" + curId);
-    newIdDiv.setAttribute('class', 'speDiv');
-    newInfFromDiv.setAttribute('id', "infFrom" + curId);
-    newInfFromDiv.setAttribute('class', 'speDiv');
-    newNotesDiv.setAttribute('id', 'notes' + curId);
-    newNotesDiv.setAttribute('class', 'speDiv');
-    newUserNameDiv.setAttribute('id', 'userName' + curId);
-    newUserNameDiv.setAttribute('class', 'speDiv');
-    newActionBtn.setAttribute('id', 'actButton');
-    newActionBtn.setAttribute('class', 'actButton');
-    newActionBtn.setAttribute('onclick', 'bringUp(' + curId + ");");
-    newActionBtn.appendChild(document.createTextNode('Otevřít'));
-    if (FData.get('status') == 'Not Active') {
-        newActionBtn.disabled = true;
-        newStatusDiv.setAttribute('class', 'notActive');
+    let curId = FData.get('id');
+    if (!idUsed.includes(curId)) {
+        idUsed.push(curId);
+        console.log(idUsed, curId)
+        const newDiv = document.createElement("div");
+        const newIdDiv = document.createElement("div");
+        const newInfFromDiv = document.createElement("div");
+        const newNotesDiv = document.createElement("div");
+        const newUserNameDiv = document.createElement("div");
+        const newActionBtn = document.createElement("button");
+        const newStatusDiv = document.createElement("div");
+        newDiv.setAttribute('id', 'hackList')
+        newIdDiv.setAttribute('id', "ID" + curId);
+        newIdDiv.setAttribute('class', 'speDiv');
+        newInfFromDiv.setAttribute('id', "infFrom" + curId);
+        newInfFromDiv.setAttribute('class', 'speDiv');
+        newNotesDiv.setAttribute('id', 'notes' + curId);
+        newNotesDiv.setAttribute('class', 'speDiv');
+        newUserNameDiv.setAttribute('id', 'userName' + curId);
+        newUserNameDiv.setAttribute('class', 'speDiv');
+        newActionBtn.setAttribute('id', 'actButton');
+        newActionBtn.setAttribute('class', 'actButton');
+        newActionBtn.setAttribute('onclick', 'bringUp(' + curId + ");");
+        newActionBtn.appendChild(document.createTextNode('Otevřít'));
+        if (FData.get('status') == 'Not Active') {
+            newActionBtn.disabled = true;
+            newStatusDiv.setAttribute('class', 'notActive');
+        } else {
+            newStatusDiv.setAttribute('class', 'Active');
+        }
+        newStatusDiv.setAttribute('id', 'status' + curId);
+        newDiv.appendChild(newIdDiv);
+        newDiv.appendChild(newInfFromDiv);
+        newDiv.appendChild(newNotesDiv);
+        newDiv.appendChild(newUserNameDiv);
+        newDiv.appendChild(newActionBtn);
+        newDiv.appendChild(newStatusDiv);
+        tabContent.appendChild(newDiv);
+        console.log("Done");
+        addType(newId('ID', curId), 'p', 'id', FData)
+        addType(newId("infFrom", curId), "p", "infFrom", FData);
+        addType(newId("notes", curId), "p", "notes", FData);
+        addType(newId("userName", curId), "p", "username", FData);
+        addType(newId("status", curId), "p", "status", FData);
     } else {
-        newStatusDiv.setAttribute('class', 'Active');
+        alert('Id ' + curId + " is already in use!")
     }
-    newStatusDiv.setAttribute('id', 'status' + curId);
-    newDiv.appendChild(newIdDiv);
-    newDiv.appendChild(newInfFromDiv);
-    newDiv.appendChild(newNotesDiv);
-    newDiv.appendChild(newUserNameDiv);
-    newDiv.appendChild(newActionBtn);
-    newDiv.appendChild(newStatusDiv);
-    tabContent.appendChild(newDiv);
-    console.log("Done");
-    addId(newId('ID', curId), 'p', curId)
-    addType(newId("infFrom", curId), "p", "infFrom", FData);
-    addType(newId("notes", curId), "p", "notes", FData);
-    addType(newId("userName", curId), "p", "username", FData);
-    addType(newId("status", curId), "p", "status", FData);
 })
 
 function addType(id, typeToAdd, formId, FData) {
@@ -69,5 +75,5 @@ function newId(og, curId) {
 }
 
 function bringUp(id) {
-    window.location.href = page + "person/person.html?id=" + id;
+    window.location.href = page + "person/person?id=" + id;
 }
